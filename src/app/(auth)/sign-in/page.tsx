7d19212d7ce5b416dbeea06987d76703ca4b9d2d
Sangyan Dayal <sangyandayal5@gmail.com>
2025-08-19 03:57:09 +0530

    04-auth-ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,16 @@
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+
+const Page = () => {
+    return <SignInView/>
+}
+
+export default Page;
+
+/* 
+
+The url to load this page is http://localhost:3000/sign-in
+
+We can see that (auth) folder should also come but it not coming as
+it is under the small brackets. That's why these brackets are used
+
+*/
